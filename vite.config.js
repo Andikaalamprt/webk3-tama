d0ac6e5cbd6a770ad6323770f69a05330265c696
Andikaalamprt <andikaalamprt2005@gmail.com>
2026-05-18 06:42:24 +0700

    web-pelatihank3-tama

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GANTI 'k3-pelatihan' dengan nama repository GitHub kamu!
   base: '/webk3-tama/',
 })
